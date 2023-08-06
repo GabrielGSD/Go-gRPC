8b277a72c7715b828c5fc3f79eb50cabf1f1a871
Gabriel de Souza <51684325+GabrielGSD@users.noreply.github.com>
2023-08-06 17:11:22 +0000

    Feat: list categories

--- internal/service/category.go
+++ internal/service/category.go
@@ -34,3 +34,26 @@ func (c *CategoryService) Create(ctx context.Context, in *pb.CreateCategoryReque
 
 	return categoryResponse, nil
 }
+
+func (c *CategoryService) List(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDb.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+	}
+
+	return &pb.CategoryList{
+		Categories: categoriesResponse,
+	}, nil
+}
